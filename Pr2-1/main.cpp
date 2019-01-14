57053252173a88fc5288ee6ea67e1caa6ee4047b
Ross Hansen <ross.hansen@inthejourney.com>
2019-01-13 19:41:21 -0600

    Initial Commit

---
+++ Pr2-1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Pr2-1
+//
+//  Created by Ross Hansen on 19/01/13.
+//  Copyright © 2019 Ross Hansen. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
